25ecbf947e8847acfb19ef630836cfc54be0320e
06madhu9 <madhumatimadalageri@gmail.com>
2025-04-15 15:18:47 +0530

    new file

---
+++ cal.java
@@ -0,0 +1,14 @@
+
+
+public class cal {
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+        System.out.println("a+b= "+(a+b));
+        System.out.println("a-b= "+(a-b));
+        System.out.println("a*b= "+(a*b));
+        System.out.println("a/b= "+(a/b));
+        System.out.println("a%b= "+(a%b));
+    }
+
+}
